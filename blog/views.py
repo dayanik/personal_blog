d6b03d5bf9905944108adeacc894686cbf098895
dayan iskhakov <dayan.iskhakov@gmail.com>
2025-08-09 11:50:03 +0300

    views refactoring

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required as django_login_required
 from django.http import JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from django.views.decorators.http import require_POST
 from django.urls import reverse
+from functools import wraps
 
 from .models import Article
+
+
+def login_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        return django_login_required(redirect_field_name='')(view_func)(request, *args, **kwargs)
+    return _wrapped_view
 
 
 def index(request):
@@ -14,16 +22,16 @@ def index(request):
 
 
 def article(request, id):
-    article = Article.objects.get(id=id)
+    article = get_object_or_404(Article, id=id)
     return render(request, 'blog/article.html', context={'article': article})
 
 
-@login_required(redirect_field_name='')
+@login_required
 def edit(request, id):
-    article = Article.objects.get(id=id)
+    article = get_object_or_404(Article, id=id)
     if request.method == 'POST':
         content = request.POST.get('content', article.content)
-        title = request.POST.get('title')
+        title = request.POST.get('title', article.title)
         article.content = content
         article.title = title
         article.save()
@@ -31,13 +39,7 @@ def edit(request, id):
     return render(request, 'blog/edit.html', context={'article': article})
 
 
-@login_required(redirect_field_name='')
-def admin(request):
-    articles = Article.objects.all()
-    return render(request, 'blog/admin.html', context={'articles': articles})
-
-
-@login_required(redirect_field_name='')
+@login_required
 def new(request):
     if request.method == 'POST':
         content = request.POST.get('content')
@@ -48,11 +50,18 @@ def new(request):
     return render(request, 'blog/new.html')
 
 
-@login_required(redirect_field_name='')
+@login_required
+def admin(request):
+    articles = Article.objects.all()
+    return render(request, 'blog/admin.html', context={'articles': articles})
+
+
+@login_required
+@require_POST
 def delete(request):
     article_id = request.POST.get('id')
     try:
-        article = Article.objects.get(id=article_id)
+        article = get_object_or_404(Article, id=article_id)
         article.delete()
         return JsonResponse({'success': True})
     except Article.DoesNotExist:
@@ -72,6 +81,7 @@ def login_admin(request):
             return JsonResponse(
                 {'success': False, 'message': 'Неверный логин или пароль'})
     return JsonResponse({'success': False, 'message': 'Недопустимый метод.'})
+
 
 @require_POST
 def logout_admin(request):
